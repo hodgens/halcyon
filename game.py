736dad554b86a1d70a1b2a1307b258282f767024
Charlie Hodgens <hodgens@email.unc.edu>
2014-03-06 13:37:15 -0500

    March 6th, 2014 I'm starting to get the GUI put together and mocked up. I have button classes for specifying their position and colors and we should be able to put functions in them, and I've left lots of planning comments in the main file.

--- game.py
+++ game.py
@@ -1,11 +1,65 @@
 #BEHOLD THE BLANK PAGE
 import re
+import pygame
+from pygame.locals import *
+from settings import *
 
-environment_file = open(sys.argv[1])
+# let's get the PyGame initialization out of the way
+(numberpassed, numberfailed) = pygame.init()
 
+main_screen = pygame.display.set_mode( [SCREEN_WIDTH, SCREEN_HEIGHT] )
+exit_status = 0 # for quitting the main loop
+center_box = pygame.Surface((20,20))
+center_box.fill((100,125,255))
+main_screen.blit(center_box, (0,0))
+
+# now for the actual Halcyon stuff
+#environment_file = open(sys.argv[1])
 SAVED_NODES = []
 
-class NodeBuffer:
+# now a dict to store the different command states for the UI
+# "open" means it hasn't been pressed.
+# "set" means it's been pressed, and will be used as the next command on submit
+# this isn't a set in stone thing, it's just a proof of concept
+KEY_COMMAND_STATUS = {K_w:"open", K_a:"open", K_s:"open", K_d:"open", K_q:"open", K_e:"open", K_f:"open"}
+
+# Since we're going to be making multiple types of buttons, with multiple instances of each type,
+# let's make a settings class we can plug into the buttons module
+# to make a new button, you just specify the settings object and give it text and x,y position (top left corner)
+class Settings:
+	def __init__(self, width, height, open_color, set_color, background_color, border_width):
+		self.width = width
+		self. height = height
+		self.open_color = open_color # used to define base color
+		self.set_color = set_color
+		self.background_color = background_color
+		self.border_width = border_width
+
+class Button:
+	def __init__(self, x_pos = None, y_pos = None, button_settings = None, text = None, action = None):
+		self.button_settings = button_settings
+		self.text = text
+		self.width = self.button_settings.width
+		self.height = self.button_settings.height
+		self.open_color = self.button_settings.open_color
+		self.set_color = self.button_settings.set_color
+		self.background_color = self.button_settings.background_color
+		self.border_width = self.button_settings.border_width
+		self.x_pos = x_pos
+		self.y_pos = y_pos
+		self.button_settings = None # how big is it, what color is it, that stuff
+		self.action = action # the function the button performs when it's pressed by the user
+		
+		if self.width is not None and self.height is not None and self.open_color is not None and self.background_color is not None and self.border_width is not None:
+			self.button_surface = pygame.Surface((self.width,self.height))
+			self.button_surface.fill(self.background_color)
+			self.button_surface.fill(self.open_color,(self.border_width, self.border_width, self.width - self.border_width, self.height - self.border_width))
+	def set_state(self,state):
+		x=1
+		# switch state from open to set, if applicable
+
+		
+class MapNode:
 	def __init__(self):
 		self.Node_contents = None # for storing all the original specifications of the Node
 		self.Clean_node_contents = []
@@ -31,11 +85,10 @@ class NodeBuffer:
 				current_story_element_name = line_search.group(1)
 			# the above will only happen if the current line contains a new element declaration
 			
-			
 			content_search = re.search(":(.+?)\]",line)
 			if content_search is None:
-				non_ending_content = re.search(":(.+?)
-				current_story_element_content += 
+				non_ending_content = re.search(":(.+?)")
+				current_story_element_content += non_ending_content
 			# the above will only happen if the current line does not contain the end of the new element declaration	
 			
 			# if you see the end character for a node block
@@ -46,8 +99,21 @@ class NodeBuffer:
 				#append
 				current_node = ""
 			# the above will only happen if the current line ends the story element delcaration
-		
-		
+			
+class AI_settings:
+	def __init__(self, style):
+		self.style=style
+			
+class Character:
+	def __init__(self, location, AI_component = None):
+		self.location = location
+		self.AI_component = AI_component
+	def move_character(self, new_location):
+		x=1
+		# check that route between current and new location exists
+		# add character to list of characters in the new location
+		# remove character from list of characters in old location
+			
 def parse_node_file(file, node_storage):
 	# read in file
 	# go through the file, parse out the different node contents
@@ -75,4 +141,62 @@ def parse_node_file(file, node_storage):
 		
 		# if you don't see an end character anywhere (in the middle of a node block)	
 		if re.search("}") is None and current_node is not None:
-			current_node.add_line(line)
+			current_node.add_line(line)	
+			
+			
+# strategy for button image handling:
+# buttons start Color1 (white, say), then when they're pressed they turn Color2 (blue?)
+# and then when you release, they turn Color3 (light blue?) to signify that that button is set to send
+# once you submit the command for that round, the buttons reset to normal
+
+direction_combat_button_settings = Settings(width=BUTTON_WIDTH, height = BUTTON_HEIGHT, open_color = BUTTON_OPEN_COLOR, set_color = BUTTON_SET_COLOR, background_color = BUTTON_BACKGROUND_COLOR, border_width = BUTTON_BORDER_WIDTH)
+
+w_button = Button(x_pos = 1, y_pos = 1, button_settings = direction_combat_button_settings, text = "w")
+
+a_button = Button(x_pos = 20, y_pos = 20, button_settings = direction_combat_button_settings, text = "a")
+
+s_button = Button(x_pos = 40, y_pos = 40, button_settings = direction_combat_button_settings, text = "s")
+
+d_button = Button(x_pos = 60, y_pos = 60, button_settings = direction_combat_button_settings, text = "d")
+	
+# main strategy for game handling:
+# tasks in map node:
+# 1. check for user input - player can examine an entity or interact with it
+# when the player does, check the entity's associated properties to see what should be done
+# 2. check the map node's associated story elements
+# go to most recent story element, display as needed
+# 3. player may initiate combat with an entity
+# if so, go to combat mode
+
+# when you first enter a node, check to see if there's hostile NPCs present
+# if there are, set combat_mode=1
+# while in combat mode, check to see if the timer flag is set, and what the time
+# every loop, check the time and make sure you're under it.
+# check for player input, and when you detect player input, start figuring out NPC combat actions
+# apply actions, re-loop
+
+# if there's not hostile NPCs, then enter map mode
+# when you enter map mode, check the current map node, and look at its stories
+# find the youngest unlocked story element, display it (unless it has a display_once flag set)
+# if there are none, default to the basic node description
+
+combat_mode = 0
+map_mode = 1
+
+# the main loop just polls the keyboard for events
+# it calls the appropriate button based on the key input
+# it's agnostic to what the buttons actually do or what's being displayed on screen
+# the buttons call handler functions that perform the proper actions mased on checking the combat_mode and map_mode flags
+while exit_status is 0:
+	next_event = pygame.event.poll()
+	if next_event == pygame.NOEVENT:
+		continue
+	if next_event.type == KEYDOWN and next_event.key == K_ESCAPE:
+		exit_status = 1
+	if next_event.type == KEYDOWN and next_event.key == K_c:
+		center_box.fill((100,200,100))
+		main_screen.blit(center_box, (0,0))
+		
+	pygame.display.flip() #flip updates the main_screen to the actual displayscreen
+
+pygame.quit()
